db7e93782073735c9d29b6906024aeb1514e1052
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-22 13:00:48 -0700

    up to freq

--- custom-sorts.js
+++ custom-sorts.js
@@ -13,18 +13,47 @@ function ageSort(users) {
 function oddEvenSort(arr) {
   let odd = arr.filter(ele=>{return ele%2!==0})
   let even = arr.filter(ele=>{return ele%2===0})
+  odd.sort((a,b)=>a-b);
+  even.sort((a, b) => a - b);
+  return odd.concat(even);
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  let string = s.split("");
+  let variable = t.split("");
+  string = string.sort().join("")
+  variable = variable.sort().join("")
+  return string===variable
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  let trip = [];
+  let double = [];
+  let single = [];
+  arr.forEach(ele=>{
+    if(ele<10){
+      single.push(ele);
+    } else if(ele>9&&ele<100){
+      double.push(ele)
+    } else trip.push(ele)
+  })
+
+  trip.sort((a,b)=>a-b);
+  double.sort((a,b)=>a-b);
+  single.sort((a,b)=>a-b);
+
+  return [...trip,...double,...single]
 }
 
 function frequencySort(arr) {
-  // Your code here
+  let freq = {};
+  arr.forEach(ele=>{
+    if(freq[ele]===undefined){
+      freq[ele]=1
+    } else freq[ele]++;
+  })
+  const valArr = Object.entries(freq)
+  console.log(valArr)
 }
 
 module.exports = [
